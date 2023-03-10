bf16eee12589e9dc939ccef5ca7cf4d772e7909e
Gloria Gideon <gloriagi@il-e6.msk.21-school.ru>
2023-03-10 18:31:50 +0300

    change view on materialized view, add triggers

--- src/part2/part2.4_groups.sql
+++ src/part2/part2.4_groups.sql
@@ -1,20 +1,41 @@
--- find GROUP_ID
+-- GROUP_ID
 
-WITH sku_info AS (SELECT DISTINCT personal_data.customer_id,
-                         checks.sku_id,
+CREATE MATERIALIZED VIEW group_id AS
+  SELECT DISTINCT personal_data.customer_id,
                          sku.group_id
                     FROM personal_data INNER JOIN cards ON personal_data.customer_id = cards.customer_id
                     INNER JOIN transactions ON cards.customer_card_id = transactions.customer_card_id
                     INNER JOIN checks ON transactions.transaction_id = checks.transaction_id
-                    INNER JOIN sku ON checks.sku_id = sku.sku_id)
+                    INNER JOIN sku ON checks.sku_id = sku.sku_id
+    ORDER BY 1, 2;
 
+CREATE OR REPLACE FUNCTION refresh_mat_view()
+RETURNS TRIGGER LANGUAGE plpgsql
+AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW group_id;
+    RETURN NULL;
+end $$;
 
+CREATE TRIGGER refresh_mat_view_personal_data
+AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+ON personal_data FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_mat_view();
 
-SELECT DISTINCT customer_id,
-                group_id
-FROM sku_info
-ORDER BY 1, 2;
+CREATE TRIGGER refresh_mat_view_transactions
+AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+ON transactions FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_mat_view();
 
---
+CREATE TRIGGER refresh_mat_view_checks
+AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+ON checks FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_mat_view();
 
-select * from periods
+CREATE TRIGGER refresh_mat_view_sku
+AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE
+ON sku FOR EACH STATEMENT
+EXECUTE PROCEDURE refresh_mat_view();
+
+SELECT * FROM group_id;
+
